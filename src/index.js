30eb0dace7ab661e0454b59b2512d05c8a3a3958
mrdatngo <mrdatngo@gmail.com>
2020-06-16 04:57:19 +0700

    reactjs2002 reactjs day 5

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import RandomGame, { App, App2 } from './App';
 import Caculate from './caculate/Caculate'
 import JsxExtend from './jsxextend/JsxExtend'
+import Parent from './lifecyle/Parent'
 import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render( <RandomGame user_name="admin" />, document.getElementById('root'));
 // ReactDOM.render( <Caculate />, document.getElementById('root'));
-ReactDOM.render( <JsxExtend />, document.getElementById('root'));
+ReactDOM.render( <Parent />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
